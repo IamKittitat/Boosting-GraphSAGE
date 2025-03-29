4843f2b5643adf1891708a300d74e81e255ae4f0
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2025-03-29 18:32:22 +0700

    fix: update lanta_tunescript.sh for compute partition and reduce CPUs

--- scripts/lanta_tunescript.sh
+++ scripts/lanta_tunescript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
-#SBATCH -p memory                 
+#SBATCH -p compute
 #SBATCH -N 1              
-#SBATCH --cpus-per-task=32        
+#SBATCH --cpus-per-task=16
 #SBATCH -t 20:00:00               
 #SBATCH --mail-user=kittitattuntisak@hotmail.com
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
